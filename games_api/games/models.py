7c0f3272dfe04bdbcdd09ed30a72391c184e1745
Nayanna Nara <nayanna501@gmail.com>
2023-07-04 10:59:53 -0300

    Add models and schemas

---
+++ games_api/games/models.py
@@ -0,0 +1,42 @@
+from typing import Type, TypeVar
+from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from games_api.contrib.models import BaseModel
+from games_api.games.schemas import GameSchema
+
+TYPE_GAME_MODEL = TypeVar('TYPE_GAME_MODEL', bound='GameModel')
+
+
+class PlayerModel(BaseModel):
+    __tablename__ = 'players'
+
+    pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(40), nullable=False)
+    game: Mapped['GameModel'] = relationship(
+        'GameModel', back_populates='players'
+    )
+
+
+class GameModel(BaseModel):
+    __tablename__ = 'games'
+
+    pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    player_pk_id: Mapped[int] = mapped_column(ForeignKey("players.pk_id"))
+    name: Mapped[str] = mapped_column(String(40), nullable=False)
+    total_kills: Mapped[int] = mapped_column(Integer, nullable=False)
+    kills: Mapped[dict[str, int]] = mapped_column(JSON)
+    players: Mapped[list['PlayerModel']] = relationship(back_populates='game', lazy='selectin', uselist=True)
+
+    @classmethod
+    def parse_schema(
+        cls: Type[TYPE_GAME_MODEL],
+        game: GameSchema,
+    ) -> 'GameModel':
+        model = GameModel(
+            **game.dict(exclude={'players'}),
+            players=[PlayerModel(**game.players.dict())]
+        )
+        
+        return model
---
+++ games_api/games/schemas.py
@@ -0,0 +1,20 @@
+from pydantic import Field
+
+from games_api.contrib.schemas import BaseSchemaMixin
+
+
+class PlayerSchema(BaseSchemaMixin):
+    name: str = Field(title='Player name', example='Isgalamido')
+
+
+class GameSchema(BaseSchemaMixin):
+    name: str = Field(..., title='Game Name', example='game_1')
+    players: list[PlayerSchema] = Field(
+        ..., title='Game players', example=["Dono da bola", "Isgalamido", "Zeh"]
+    )
+    total_kills: int = Field(..., title='Kills count', example=45)
+    kills: dict[str, int] = Field(..., title='Kills', example={
+        "Dono da bola": 5,
+        "Isgalamido": 18,
+        "Zeh": 20
+    })
