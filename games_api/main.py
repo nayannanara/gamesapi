d3c4d463700179249799c7e86dac7b116be6d0ed
Nayanna Nara <nayanna501@gmail.com>
2023-07-05 20:36:32 -0300

    adjusts

--- games_api/main.py
+++ games_api/main.py
@@ -1,9 +1,7 @@
-import re
 from fastapi import FastAPI
 
 from games_api.configs.settings import settings
 from games_api.routers import api_router
-
 
 app = FastAPI(title='Games API')
 app.include_router(api_router, prefix=settings.API_VERSION)
@@ -12,4 +10,6 @@ app.include_router(api_router, prefix=settings.API_VERSION)
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, log_level='info', reload=True)
+    uvicorn.run(
+        'main:app', host='0.0.0.0', port=8000, log_level='info', reload=True
+    )
