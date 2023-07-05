153cfcefd63587ac148e1bca76a74d86aa6a5f95
Nayanna Nara <nayanna501@gmail.com>
2023-07-05 20:29:13 -0300

    Adjust controller and usecase with Annotated

--- games_api/games/controller.py
+++ games_api/games/controller.py
@@ -1,20 +1,16 @@
-from games_api.configs.database import get_session
-
+from games_api.contrib.dependencies import DatabaseDependency
 from games_api.games.usecases import GameUseCase
 from games_api.games.schemas import GameSchema
 
 from fastapi import APIRouter, Depends, status
-
-from sqlalchemy.ext.asyncio import AsyncSession
-
 
 router = APIRouter()
 
 
 @router.get('/', status_code=status.HTTP_200_OK)
 async def query(
+    db_session: DatabaseDependency,
     use_case: GameUseCase = Depends(),
-    db_session: AsyncSession = Depends(get_session),
 ) -> list[GameSchema]:
     games = await use_case.query(db_session=db_session)
 
--- games_api/games/usecases.py
+++ games_api/games/usecases.py
@@ -1,17 +1,12 @@
-from games_api.configs.database import get_session
+from games_api.contrib.dependencies import DatabaseDependency
 from games_api.games.schemas import GameSchema
 from games_api.games.models import GameModel
 
-from fastapi import Depends
-
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql import select
 
 
 class GameUseCase:
-    async def query(
-        self: 'GameUseCase', db_session: AsyncSession = Depends(get_session)
-    ) -> list[GameSchema]:
+    async def query(self: 'GameUseCase',db_session: DatabaseDependency) -> list[GameSchema]:   
         games = (await db_session.execute(select(GameModel))).scalars().all()
         
         return [GameSchema.from_orm(game) for game in games]
